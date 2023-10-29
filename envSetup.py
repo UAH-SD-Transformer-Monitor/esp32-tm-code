73b73016747f402945f1b1491b64911055403c40
Andrew Woodlee <cybershell@cybershell.xyz>
2023-10-28 21:16:58 -0500

    change mqtt_port to string in envSetup.py change and add libraries

--- envSetup.py
+++ envSetup.py
@@ -27,7 +27,7 @@ import ssl
 sslEnabled = os.getenv(envVars["SSL_ENABLED"])
 mqttPort = 1083
 if sslEnabled != "enabled" or sslEnabled == None:
-  os.environ[envVars["MQTT_PORT"]] = 1083
+  os.environ[envVars["MQTT_PORT"]] = "1083"
    
 
 wifiSSID = os.getenv(envVars["WIFI_SSID"])
